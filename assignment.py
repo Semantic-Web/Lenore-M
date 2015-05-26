b732f43851901e9d44f5ac8612108fb7c7d21096
lenore56 <lmontalbano>
2015-05-26 15:37:25 -0400

    removed files

--- assignment.py
+++
@@ -1,9 +0,0 @@
-import csv
-from collections import defaultdict
-reader = csv.DictReader(open('C:\Users\Lenore\Desktop\School\MSITM\Python\datagovdatasetsviewmetrics.csv'))
-organizations = defaultdict(int)
-for row in reader:
-    organizations[row["Organization Name"]] += int(row["Views per Month"])
-    
-for organization, views in sorted(organizations.iteritems(), key=lambda (k, v): (-v, k))[:10]:
-    print organization, views
